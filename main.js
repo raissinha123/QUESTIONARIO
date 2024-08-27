ea97635c9081d304302bc9cd81011ed75cc649e2
raissinha123 <147162333+raissinha123@users.noreply.github.com>
2024-08-27 11:02:49 -0300

    ççç

--- main.js
+++ main.js
@@ -3,3 +3,25 @@ const caixaPergunta = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+
+const perguntas = [
+{
+    enunciado: "Pergunta 1"
+    alternativas: ["alternativa1", "alternativa2", "alternativa3"]
+}
+{
+    enunciado: "Pergunta 2"
+    alternativas: ["alternativa1", "alternativa2", "alternativa3"]
+}
+{
+    enunciado: "Pergunta 3"
+    alternativas: ["alternativa1", "alternativa2", "alternativa3"]
+}
+{
+    enunciado: "Pergunta 4"
+    alternativas: ["alternativa1", "alternativa2", "alternativa3"]
+}
+
+
+]
