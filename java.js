ae516307ab001f4459448970670347d045af9915
raissinha123 <147162333+raissinha123@users.noreply.github.com>
2024-08-27 07:48:20 -0300

    nnn

---
+++ java.js
@@ -0,0 +1,4 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document. querySelector(".caixa-perguntas");
+const caixaAlternativa = document. querySelector(".caixa-alternativa");
+const caixaResultado
