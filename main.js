c4e41d2be4a7adad05605aa7aaca3e9e25bf271b
raissinha123 <147162333+raissinha123@users.noreply.github.com>
2024-08-27 11:06:13 -0300

    ççç

--- main.js
+++ main.js
@@ -9,19 +9,19 @@ const perguntas = [
 {
     enunciado: "Pergunta 1"
     alternativas: ["alternativa1", "alternativa2", "alternativa3"]
-}
+},
 {
     enunciado: "Pergunta 2"
     alternativas: ["alternativa1", "alternativa2", "alternativa3"]
-}
+},
 {
     enunciado: "Pergunta 3"
     alternativas: ["alternativa1", "alternativa2", "alternativa3"]
-}
+},
 {
     enunciado: "Pergunta 4"
     alternativas: ["alternativa1", "alternativa2", "alternativa3"]
-}
+},
 
 
 ]
